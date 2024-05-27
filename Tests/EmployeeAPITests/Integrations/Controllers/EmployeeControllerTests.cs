7558a18cbceda182dad216a3d6bc95120f95e405
jelainemartins <jcm210891@gmail.com>
2024-05-27 12:00:24 -0300

    Configuração do ambiente de testes;

--- Tests/EmployeeAPITests/Integrations/Controllers/EmployeeControllerTests.cs
+++ Tests/EmployeeAPITests/Integrations/Controllers/EmployeeControllerTests.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ikvm.runtime;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace EmployeeAPITests.Integrations.Controllers
 {
-    internal class EmployeeControllerTests
+    public class EmployeeControllerTests
     {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public EmployeeControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
     }
 }
