5423bf3045f2f3796486a4dcf56b6492f7cfb495
jelainemartins <jcm210891@gmail.com>
2024-05-31 15:52:17 -0300

    Configuração do Program; API valida o token;

--- EmployeeAPI/Program.cs
+++ EmployeeAPI/Program.cs
@@ -1,8 +1,12 @@
 using System.Reflection;
+using System.Text;
+using EmployeeAPI;
 using EmployeeAPI.Context;
 using EmployeeAPI.Interface;
 using EmployeeAPI.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +20,24 @@ builder.Services.AddSwaggerGen();
 
 //Injeção de dependência;
 builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+
+var key = Encoding.ASCII.GetBytes(Key.Secret);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 var app = builder.Build();
 
