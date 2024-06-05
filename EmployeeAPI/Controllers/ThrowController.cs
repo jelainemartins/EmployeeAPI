3d0a2a52f61d9d010d4690e5a7cca9979e474878
jelainemartins <jcm210891@gmail.com>
2024-06-05 15:48:14 -0300

    Adicionando o Handler Error;

---
+++ EmployeeAPI/Controllers/ThrowController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI.Controllers
+{
+    /// Anotação que irá ignorar como API;
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ThrowController : Controller
+    {
+        [Route("/error")]
+        public IActionResult HandlerError() => Problem();
+
+        [Route("/error-development")]
+        public IActionResult HandlerErrorDevelopment([FromServices] IHostEnvironment hostingEnvironment)
+        {
+            if (!hostingEnvironment.IsDevelopment())
+            {
+                return NotFound();
+            }
+            var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()!;
+            return Problem(detail: exceptionHandlerFeature.Error.StackTrace, title: exceptionHandlerFeature.Error.Message);
+        }
+    }
+}
