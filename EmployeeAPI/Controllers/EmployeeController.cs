0b8493e077d5c49935d4bd09d21028fe7c2aa440
jelainemartins <jcm210891@gmail.com>
2024-06-05 15:47:36 -0300

    Adicionando o _logger; Dois modelos;

--- EmployeeAPI/Controllers/EmployeeController.cs
+++ EmployeeAPI/Controllers/EmployeeController.cs
@@ -10,15 +10,19 @@ namespace EmployeeAPI.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly ILogger<EmployeeController> _logger;
 
-        public EmployeeController(IEmployeeRepository employeeRepository)
+        public EmployeeController(IEmployeeRepository employeeRepository, ILogger<EmployeeController> logger)
         {
-            _employeeRepository = employeeRepository ?? throw new ArgumentNullException();
+            _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
+
 
         [HttpPost]
         public IActionResult Add(EmployeeViewModel employeeViewModel)
         {
+            _logger.LogInformation($"Add {employeeViewModel}");
             var employee = new Employee(employeeViewModel.FirstName, employeeViewModel.LastName, employeeViewModel.Birthdate, employeeViewModel.Sex, employeeViewModel.Job, null);
             _employeeRepository.Add(employee);
             return CreatedAtAction(nameof(GetId), new { id = employee.Id }, employee);
@@ -27,12 +31,14 @@ namespace EmployeeAPI.Controllers
         [HttpPost]
         public IActionResult AddList(List<Employee> employeeList)
         {
+            _logger.LogInformation($"AddList {employeeList}");
             return Ok(_employeeRepository.AddList(employeeList));
         }
 
         [HttpGet]
         public IActionResult GetId(int id)
         {
+            _logger.LogInformation($"GetId {id}");
             var employee = _employeeRepository.GetById(id);
             return employee == null ? NotFound() : Ok(employee);
         }
@@ -40,6 +46,7 @@ namespace EmployeeAPI.Controllers
         [HttpGet]
         public IActionResult Get()
         {
+            _logger.LogInformation("Get");
             var employees = _employeeRepository.Get();
             return Ok(employees);
         }
@@ -48,12 +55,22 @@ namespace EmployeeAPI.Controllers
         public IActionResult GetBySex(char sex)
         {
             var employees = _employeeRepository.GetBySex(sex);
-            return Ok(employees);
+            if (employees != null && employees.Count > 0)
+            {
+                _logger.LogInformation($"GetBySex {sex}");
+                return Ok(employees);
+            }
+            else
+            {
+                _logger.Log(LogLevel.Error, "Ocorreu um erro");
+                return NotFound();
+            }
         }
 
         [HttpGet]
         public IActionResult GetByJob(string job)
         {
+            _logger.LogInformation($"GetByJob {job}");
             var employees = _employeeRepository.GetByJob(job);
             return Ok(employees);
         }
@@ -61,6 +78,7 @@ namespace EmployeeAPI.Controllers
         [HttpDelete]
         public IActionResult Delete(int id)
         {
+            _logger.LogInformation($"Delete {id}");
             _employeeRepository.Remove(id);
             return Ok();
         }
