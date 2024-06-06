f8ce0567365c40e203c6bcb792c7044ad7435a58
jelainemartins <jcm210891@gmail.com>
2024-06-06 16:53:07 -0300

    [EMPLOYEE VIEW MODEL] [IFormFile] Criado para o [Photo];

--- EmployeeAPI/ViewModel/EmployeeViewModel.cs
+++ EmployeeAPI/ViewModel/EmployeeViewModel.cs
@@ -1,13 +1,13 @@
-﻿using EmployeeAPI.Models;
-
+﻿
 namespace EmployeeAPI.ViewModel
 {
     public class EmployeeViewModel
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime Birthdate { get; set; }
+        public DateTime? Birthdate { get; set; }
         public string Job { get; set; }
-        public char Sex { get; set; }
+        public char? Sex { get; set; }
+        public IFormFile Photo { get; set; }
     }
 }
