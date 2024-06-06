a647e3bc3d7deee277c11b1cc57796cfe462f66b
jelainemartins <jcm210891@gmail.com>
2024-06-06 16:49:36 -0300

    [REPOSITORY] [GetByJob] Implementação; [Update] Implementação; Utilizando PathCombine; O [UPDATE] Atualiza todos os campos e mantém os valores enviados como [null];

--- EmployeeAPI/Repositories/EmployeeRepository.cs
+++ EmployeeAPI/Repositories/EmployeeRepository.cs
@@ -1,9 +1,13 @@
 ﻿using EmployeeAPI.Context;
 using EmployeeAPI.Interface;
+using EmployeeAPI.Migrations;
 using EmployeeAPI.Models;
+using EmployeeAPI.ViewModel;
 
 namespace EmployeeAPI.Repositories
 {
+    //Usando a interface de herança [IEmployeeRepository];
+    //Tudo que há na [IEmployeeRepository], será herdado obrigatoriamente pelo [REPOSITORY];
     public class EmployeeRepository : IEmployeeRepository
     {
         private readonly EmployeeContext _context;
@@ -34,6 +38,10 @@ namespace EmployeeAPI.Repositories
         {
             return _context.Employees.Where(employee => employee.Sex == sex).ToList();
         }
+        public List<Employee> GetByJob(string job)
+        {
+            return _context.Employees.Where(employee => employee.Job == job).ToList();
+        }
 
         public List<Employee> AddList(List<Employee> employeeList)
         {
@@ -51,9 +59,31 @@ namespace EmployeeAPI.Repositories
             }
         }
 
-        public List<Employee> GetByJob(string job)
+        public Employee Update(int id, EmployeeViewModel employeeViewModel)
         {
-            return _context.Employees.Where(employee => employee.Job == job).ToList();
+            ///Buscando Employee por ID
+            var employee = GetById(id);
+            ///Verifica se esta nulo
+            if (employee != null)
+            {
+                var filePath = string.Empty;
+                if (employeeViewModel.Photo != null)
+                {
+                    filePath = Path.Combine("Storage", employeeViewModel.Photo.FileName);
+                    if (filePath != employee.Photo)
+                    {
+                        File.Delete(employee.Photo);
+                    }
+                    using Stream fileStream = new FileStream(filePath, FileMode.Create);
+                    employeeViewModel.Photo.CopyTo(fileStream);
+                }
+                var newEmployee = new Employee(id, employeeViewModel.FirstName ?? employee.FirstName, employeeViewModel.LastName ?? employee.LastName, employeeViewModel.Birthdate ?? employee.Birthdate, employeeViewModel.Sex ?? employee.Sex, employeeViewModel.Job ?? employee.Job, string.IsNullOrEmpty(filePath) ? employee.Photo : filePath);
+                _context.ChangeTracker.Clear();
+                _context.Employees.Update(newEmployee);
+                _context.SaveChanges();
+                return newEmployee;
+            }
+            throw new Exception($"{id} nao encontrado.");
         }
     }
 }
